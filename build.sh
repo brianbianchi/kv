cd93a79c2faee5c3aa27365e124ee5957bc01394
brianbianchi <bianchibrianj@gmail.com>
2021-12-09 22:27:05 -0500

    Put build in bin

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ killall kv || true
 sleep 0.1
 
 go install -v
-go build
+go build -o bin
 
-./kv -dbpath=./tmp/nyc.db -addr=127.0.0.1:8080 -config=sharding.toml -shard=nyc &
-./kv -dbpath=./tmp/denver.db -addr=127.0.0.1:8081 -config=sharding.toml -shard=denver &
-./kv -dbpath=./tmp/sd.db -addr=127.0.0.1:8082 -config=sharding.toml -shard=sd &
+./bin/kv -dbpath=./tmp/nyc.db -addr=127.0.0.1:8080 -config=sharding.toml -shard=nyc &
+./bin/kv -dbpath=./tmp/denver.db -addr=127.0.0.1:8081 -config=sharding.toml -shard=denver &
+./bin/kv -dbpath=./tmp/sd.db -addr=127.0.0.1:8082 -config=sharding.toml -shard=sd &
 
 wait
